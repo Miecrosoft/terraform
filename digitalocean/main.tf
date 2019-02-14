a23e17cf71ee62a5665b9ee0096162449324f394
Miecrosoft <herberts007@gmail.com>
2019-02-14 10:19:32 +0700

    Added additional value 1 after count meta-parameter

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_droplet" "web" {
   count = 5 /* how many droplets generated */
   image  = "${var.image_instance}"
-  name   = "${var.name_instance}-${count.index}" /* looping index started from 0 to 'count' */
+  name   = "${var.name_instance}-${count.index + 1}" /* looping index started from 1 to 'count' */
   region = "${var.region_instance}"
   size   = "${var.size_instance}"
 }
